ad8d8f7ac6c09706092a38c833250392872cbd1a
jainam30 <jainamjainrj03@gmail.com>
2025-12-13 15:47:26 +0530

    Fix: Add missing routes for Settings and ReviewQueue

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -61,6 +61,8 @@ export default function App() {
               <Route path="/profile/:userId" element={<ProtectedRoute><Layout><UserProfile /></Layout></ProtectedRoute>} />
               <Route path="/account" element={<ProtectedRoute><Layout><Account /></Layout></ProtectedRoute>} />
               <Route path="/friends" element={<ProtectedRoute><Layout><Friends /></Layout></ProtectedRoute>} />
+              <Route path="/settings" element={<ProtectedRoute><Layout><Settings /></Layout></ProtectedRoute>} />
+              <Route path="/review-queue" element={<ProtectedRoute><Layout><ReviewQueue /></Layout></ProtectedRoute>} />
 
               <Route path="*" element={<Navigate to="/" replace />} />
             </Routes>
